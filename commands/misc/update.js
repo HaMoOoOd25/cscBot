4ad2e068161ae34f4b816ad2e49cb86d9a77ca9d
BuildTools <unconfigured@null.spigotmc.org>
2019-06-29 12:13:56 +0300

    Update/Changelog command added
    Type .update / .changelog to post the changes commited to the bot in a channel.

---
+++ commands/misc/update.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const errors = require("../../utils/errors");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args, messageArray) => {
+
+    //If not me <3
+    if (!message.author.id === "279224191671205890") return errors.noPermissionError(message);
+
+    await message.delete();
+
+    const changeLogText = fs.readFileSync('./changelog.txt').toString('utf-8');
+    const changelogEmbed = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor(bot.settings.embedColor)
+        .addField(`${bot.user.username} Update!`, "We've got some new features for you!")
+        .addField('Overview', changeLogText)
+        .setFooter(bot.user.username, bot.user.avatarURL)
+        .setTimestamp(message.createdAt);
+    message.channel.send(changelogEmbed)
+
+};
+
+module.exports.config = {
+    name : "update",
+    usage: "update",
+    description: "This command can only be used by HaMoOoOd25#7712. It show's the bot changelogs",
+    aliases: ["changelog"],
+    permission: [],
+    enabled: true
+};
